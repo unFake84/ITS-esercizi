6043bc5678877efc099167c8aa2c97ff5639def8
unFake84 <nat_z@hotmail.it>
2025-02-19 13:06:41 +0100

    ripr7

--- EserciziDatoOperColl/esercizi1602.py
+++ EserciziDatoOperColl/esercizi1602.py
@@ -11,29 +11,42 @@
 # title = nome.title()
 # print(f"{lowercase}, {uppercase}, {title}")
 
+# # #  metodo alternativo
+# # print(nome.lower()) 
+# # print(nome.upper())
+# # print(nome.title())
+
 
 # # 2-5: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks: Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 # # 2-6: Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. Then compose your message and represent it with a new variable called message. Print your message
-# autore: str = "Kit carson"
+# autore: str = "Kit Carson"
 # cit: str = "Mai fasciarsi la testa prima di essersela rotta"
-# print(f'{autore} una volta disse :"{cit}"') # le apici vanno in conflitto tra di loro se uguali, usare \"##\" per forzare le virgolette
-
+# print(f'{autore} una volta disse: "{cit}"') # le apici vanno in conflitto tra di loro se uguali, usare \" ... \" per forzare le virgolette
+# # print(f"{autore} una volta disse: \"{cit}\"") 
 
 # # 3-1: Names: Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time
 # names: list = ["a", "b", "c", "d", "e"]
 # for name in names:
 #     print(name)
-   
+
+# names: list = ["a", "b", "c"]
+# print(names[0])
+# print(names[1])
+# print(names[2])
 
 # # 3-2: Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name
 # names: list = ["a", "b", "c", "d", "e"]
 # for name in names:
 #     print(f"{name}, no f")
-   
+
+# names: list = ["a", "b", "c"]
+# print("ciao " + names[0])
+# print("ciao " + names[1])
+# print("ciao " + names[2])
+
 
 # # 3-3: Your Own List: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 # moto: list = ["Ducati", "Yamaha", "Honda"]
-
 
 # for i in range(len(moto)):
 #     print(f"La {moto[i]}, è la mia {i+1}° preferita")
@@ -42,6 +55,10 @@
 ## print(f"La mia seconda moto preferita è, {moto[1]}")
 ## print(f"La mia terza moto preferita è, {moto[2]}")
 
+# dream_list:list[str] = ["Honda Civic", "BMW m2", "Aprilia"]
+# print("Vorrei una " + dream_list[0])
+# print("Vorrei un " + dream_list[1])
+# print("Vorrei una " + dream_list[2])
 
 # # 3-4: Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner
 # dinner: list = ["Chopin", "Mozart", "Bach"]
@@ -49,6 +66,10 @@
 # for i in dinner:
 #     print(f" Sig. {i}, è invitato a cena!")
     
+# dinner: list = ["Chopin", "Mozart", "Bach"]
+# print("Sei invitato alla cena " + dinner[0])
+# print("Sei invitato alla cena " + dinner[1])
+# print("Sei invitato alla cena " + dinner[2])
 
 # # 3-5: Changing Guest List:
 # import time
@@ -173,8 +194,10 @@
 #     print(f"Il sig. {i} è ancora invitato a cena")
 #     time.sleep(1)
     
-# del dinner
-# dinner = []
+# del dinner[1]
+# del dinner[0]
+# # del dinner # soluzione sbagliata
+# # dinner = []
 # time.sleep(1)
 # print(f"{dinner} La lista è vuota")
 
@@ -235,21 +258,21 @@
 
 # print(f"{len(dinner)} persone sono state ivitate a cena")
 
-# 3-10: Every Function:
-# Creo lista vuota
-list = []
+# # 3-10: Every Function:
+# # Creo lista vuota
+# list = []
 
-# Imposto che il limite massimo di input è 5
-for i in range(5):
+# # Imposto che il limite massimo di input è 5
+# for i in range(5):
 
-    # Faccio inserire all'utente 5 preferenze
-    user: str = input(f"\nInserire una preferenza {i+1}/5: \n")
-    user = list.append(user)
+#     # Faccio inserire all'utente 5 preferenze
+#     user: str = input(f"\nInserire una preferenza {i+1}/5: \n")
+#     user = list.append(user)
 
-# apro un ciclo di opzioni
-while True:
+# # apro un ciclo di opzioni
+# while True:
 
-    # Chiedo all'utente cosa vuole fare
-    risposta: str = input("\nCosa vuoi fare ora?\n Inserire un opzione tra:\n1Uppercase, Lowercase,")
+#     # Chiedo all'utente cosa vuole fare
+#     risposta: str = input("\nCosa vuoi fare ora?\n Inserire un opzione tra:\n1Uppercase, Lowercase,")
     
 
