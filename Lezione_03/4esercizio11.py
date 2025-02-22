b80adb88e364a98673ceaa6735037bfe9934871f
unFake84 <nat_z@hotmail.it>
2025-02-22 15:04:31 +0100

    ripr11

--- Lezione_03/4esercizio11.py
+++ Lezione_03/4esercizio11.py
@@ -1,18 +1,20 @@
-# un pò di lambridge
+# un pò di umbridge
 print('\n')
 
-# riprendo lista pers es4-1 e creo una lista nuova, uso funzione .APPEND per aggiungere un elemento a ciascuna lista
+# riprendo lista pers es4-1 e creo una lista nuova,poi uso funzione .APPEND per aggiungere un elemento a ciascuna lista
 pizza: list[str] = ["Bufala", "Margherita", "Capricciosa"]
 friend_pizza: list[str] = ["Diavola", "Marinara", "Napoli"]
+
 pizza.append("Boscaiola")
 friend_pizza.append("4Formaggi")
 
 # dimostro che la mia lista è aggiornata usando il ciclo FOR
 print('My favorite pizzas are:\n')
 for mio in pizza:
+
     print(mio)
 
-# lambridge
+# umbridge
 print('')
 print('And')
 print('')
@@ -22,5 +24,5 @@ print('My friend\'s favorite pizzas are:\n')
 for suo in friend_pizza:
     print(suo)
 
-# lmb
+# umb
 print('\n')
---
+++ Lezione_03/5lezione1.py
@@ -0,0 +1,46 @@
+# riporto esempio dell'esercizio 5-1
+# imposta la variabile car = subaru
+car = 'subaru'
+
+# stampa la frase di previsione
+print("Is car == 'subaru'? I predict True.")
+
+# effettua il controllo se car è simile a subaru (ovvero la variabile assegnata)
+print(car == 'subaru')
+
+#stampa la frase di previzione
+print("\nIs car == 'audi' I predict False.")
+
+# effettua il controllo se car è simile a subaru (ovvero la variabile assegnata)
+print(car == 'audi')
+
+# 1
+three: int = 3
+print("Is three == 3?\nI predict True.")
+print(three == 3)
+print("Is three == 2?\nI predict False")
+print(three == 2)
+
+#2
+print("Is three > 2?\nI predict True.")
+print(three > 2)
+print("Is three < 2?\nI predict False.")
+print(three < 2)
+
+#3
+print("Is three * three = 9?\nI predict True.")
+print(three * three)
+print("Is three != 3?\nI predict False.")
+
+#4
+print("Is three odd?\nI predict True.")
+print(three%1 == 0)
+print("Is three even?\nI predict False.")
+print(three%2 == 0)
+
+#5
+moon = 'Sun'
+print("If it is night, there is the moon")
+print(moon != 'sun')
+print("If is is day, there is the moon")
+print(moon == 'sun')
