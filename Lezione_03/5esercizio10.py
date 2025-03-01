558a9b37602ef659a0a22b886e568db6a2ea10f1
unFake84 <nat_z@hotmail.it>
2025-03-01 16:26:05 +0100

    om1625

--- Lezione_03/5esercizio10.py
+++ Lezione_03/5esercizio10.py
@@ -1,10 +1,15 @@
 #current_users: list[str] = ["Alpha", "Beta", "Charlie", "Delta", "Echo"]
 current_users: list[str] = ["alpha", "beta", "charlie", "delta", "echo"]
-new_users: list[str] = ["Foxtrot", "Alpha", "Golf", "Echo", "India", "BETA"]
+new_users: list[str] = ["Foxtrot", "Alpha", "Golf", "Echo", "India", "BETA", "deLTa"]
+
+print("|-----------------------------------------------------------------------|")
+print(f"|[[{current_users}]]                      |")
+print(f"|[[{new_users}]]     |")
+print("|-----------------------------------------------------------------------|")
 
 for user in new_users:
 
-    if user.lower() in current_users or user.title() in current_users:
+    if user.lower() in current_users:
 
         print(f"[{user}] username already been used, please change it.")
 
