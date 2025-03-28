1f53afd6612ef3ecd8114bf73a65717645bf7fab
unFake84 <nat_z@hotmail.it>
2025-03-28 20:45:48 +0100

    om2045

--- Lezione_05/turtleandhare.py
+++ Lezione_05/turtleandhare.py
@@ -13,7 +13,7 @@ def tartaruga(pos: int = 1):
 
     elif mossa >= 6 and mossa <= 7:
 
-        # scivolata, arretra 6 quadrati (no sotto l'1)
+        # scivolata, arretra 6 quadrati
         pos -= 6
 
     elif mossa >= 8:
@@ -44,7 +44,7 @@ def lepre(pos: int = 1):
 
     elif mossa >= 8 and mossa <= 10:
 
-        # arretra di 2 quadrati (no sotto l'1), piccola scivolata
+        # arretra di 2 quadrati, piccola scivolata
         pos -= 2
 
     return pos
@@ -58,10 +58,12 @@ def gara(pos_t: int, pos_l: int):
     print("LET THE GAME BEGIN!!!")
     time.sleep(0.8)
     os.system('clear')
+
     print("READY!!!")
     print("         _\n     .-./*)\n   _/___\/\n     U U\n")
     time.sleep(0.8)
     os.system('clear')
+
     print("SET!!!")
     print("""
        _
@@ -73,11 +75,14 @@ def gara(pos_t: int, pos_l: int):
         """)
     time.sleep(0.8)
     os.system('clear')
+
     print("BANG !!!!! AND THEY'RE OFF !!!!!")
+
     partenza: list[str] = ['_'] * 69
+
     print('Giocata n', giocata, '|', 'Tarta mossa', pos_t, '|', 'Lepre mossa', pos_l)
     print(f"1°[Tarta pos]= {posizione_tartaruga}\n2°[Lepre pos]= {posizione_lepre}\n")
-    print('TL', *partenza, '\n' * 2)
+    print('TH', *partenza, '\n' * 2)
     print("""
     BBBB  AAAA  N   N  GGGGG   MM MM
     B  B  A  A  NN  N  G       MM MM
@@ -95,11 +100,14 @@ def gara(pos_t: int, pos_l: int):
 
         pos_t = tartaruga() -1
         pos_l = lepre() -1
+
         print("!!WHO WILL COME FIRST!!?")
         print('Giocata n', giocata, '|', 'Tarta mossa', pos_t, '|', 'Lepre mossa', pos_l)
 
         posizione_tartaruga += pos_t
         posizione_lepre += pos_l
+        max_tarta: int = posizione_tartaruga
+        max_lepre: int = posizione_lepre
 
         if posizione_tartaruga == posizione_lepre and posizione_tartaruga < 70 and posizione_lepre < 70:
 
@@ -109,6 +117,7 @@ def gara(pos_t: int, pos_l: int):
 
             time.sleep(1)
             os.system('clear')
+
             print("IT'S A TIE.")
             print('[Tarta pos]=',posizione_tartaruga,'\n', '[Lepre pos]=', posizione_lepre)
             print("""
@@ -118,13 +127,40 @@ D   D  RRRR   AAAAA  W W W
 D   D  R  R   A   A  WW WW
 DDDD   R   R  A   A  W   W
                   """)
-            break
+            
+            interruttore: int = 1
+            while interruttore == 1:
+                
+                    user: str = (input("Ricominciare?: "))
+
+                    if user == 'no':
+                    
+                        interruttore = 0                    
+
+                    elif user != 'si':
+
+                        print("Risposta non valida, per chiudere digitare si.")
+
+                    elif user == 'si':
+
+                        interruttore = 0
+                    
+            if interruttore == 0 and user == 'no':
+
+                break
+
+            elif interruttore == 0 and user == 'si':
+
+                posizione_tartaruga = 1
+                posizione_lepre = 1
 
         elif posizione_tartaruga >= 70:
 
             posizione_tartaruga = 70
+
             time.sleep(1)
             os.system('clear')
+
             print("TORTOISE WINS! || VAY!!!")
             print('1° posizione Tartaruga =',posizione_tartaruga, '\n', '2° posizione Lepre =', posizione_lepre)
             print("""
@@ -137,13 +173,16 @@ DDDD   R   R  A   A  W   W
    (_..)--(.._)'--'    []
 
 """)
+            print(f"Risultato finale: {max_tarta} vs {max_lepre}")
             break
 
         elif posizione_lepre >= 70:
 
             posizione_lepre = 70
+
             time.sleep(1)
             os.system('clear')
+
             print("HARE WINS || YUCH!!!")
             print('1° posizione: Lepre =',posizione_lepre, '\n', '2° posizione: Tartaruga =', posizione_tartaruga)
             print("""
@@ -155,24 +194,25 @@ DDDD   R   R  A   A  W   W
        ((_v.)      ||
         > "<       []
 """)
+            print(f"Risultato finale: {max_lepre} vs {max_tarta}")
             break
         
         elif posizione_tartaruga < 1:
 
             posizione_tartaruga = 1
             pista[posizione_tartaruga - 1] = 'T'
-            pista[posizione_lepre - 1] = 'L'
+            pista[posizione_lepre - 1] = 'H'
 
         elif posizione_lepre < 1:
 
             posizione_lepre = 1
-            pista[posizione_lepre - 1] = 'L'
+            pista[posizione_lepre - 1] = 'H'
             pista[posizione_tartaruga - 1] = 'T'
 
         elif posizione_tartaruga != posizione_lepre:
 
             pista[posizione_tartaruga - 1] = 'T'
-            pista[posizione_lepre - 1] = 'L'
+            pista[posizione_lepre - 1] = 'H'
 
         #print('Tarta',pos_t,'\n', 'Lepre', pos_l)
         if giocata:
