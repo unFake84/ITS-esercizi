74b0ceb21f5661d81274b3c801cab4fa66f4e84e
unFake84 <nat_z@hotmail.it>
2025-03-31 12:15:27 +0200

    310325

---
+++ Lezione_06/exercise9_5.py
@@ -0,0 +1,21 @@
+'''
+Login Attempts:
+
+1.
+    Add an attribute called login_attempts to your User class from Exercise 9-3.
+    Write a method called increment_login_attempts()
+    that increments the value of login_attempts by 1.
+
+2.   
+    Write another method called reset_login_attempts()
+    that resets the value of login_attempts to 0.
+  
+3.
+    Make an instance of the User class and call increment_login_attempts() several times.
+    Print the value of login_attempts to make sure it was incremented properly,
+    and then call reset_login_attempts().
+
+4.
+    Print login_attempts again to make sure it was reset to 0.
+    
+'''
