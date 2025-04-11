acdc99387ff11ccd11dd09e581221c7178a1afc2
unFake84 <nat_z@hotmail.it>
2025-04-11 23:57:42 +0200

    120405

--- Lezione_18/esercizio2.py
+++ Lezione_18/esercizio2.py
@@ -22,14 +22,18 @@ Password Validation 1:
 
 '''
 
+import string
+
 # 1.
 def validate_password(password: str):
 
     uppercase: int = 0
+    specialcase: int = 0
 
     if len(password) < 20:
 
-        print("Error, the password must be minimum lenght of 20 characters.")
+        # 5a.
+        raise ValueError("Error, the password must be at least 20 characters long.")
 
     # 2.
     for char in password:
@@ -38,6 +42,45 @@ def validate_password(password: str):
 
             uppercase += 1
 
+        # 3.
+        if char in string.punctuation:
+
+            specialcase += 1
+
+    # 2a.
     if uppercase < 3:
 
-        print("Error, the password must be lower three uppercase letters.")
+        # 5b.
+        raise ValueError("Error, the password must contain at least three uppercase letters.")
+
+    # 3a.
+    if specialcase < 4:
+
+        # 5c.
+        raise ValueError("Error, the password must contains at least four special characters")
+
+    # 4.
+    return True
+
+if __name__ == "__main__":
+
+    print("\n\tCHANGING PASSWORD:\n")
+    print("\n\tThe password must contains at least four special characters.")
+    print("\tThe password must contain at least three uppercase letters.")
+    print("\tThe password must be at least 20 characters long.\n")
+
+    while True:
+
+        try:
+
+            user: str = (input("Insert a new password:")) # .strip()) per rimuovere gli spazi inizio&fine
+            validate_password(user)
+            break
+
+        except ValueError as fnc_error:
+
+            print(fnc_error)
+
+    print("Changing Password Complete!")
+
+    print("Done.")
