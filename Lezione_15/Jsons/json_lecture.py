787b3582719d58b35fa7f18ce62537f3d84751cc
unFake84 <nat_z@hotmail.it>
2025-05-09 18:18:20 +0200

    om1820

--- Lezione_15/Jsons/json_lecture.py
+++ Lezione_15/Jsons/json_lecture.py
@@ -1,26 +1,40 @@
 import json
 
-PATH: str = "Jsons/config.json"
+PATH: str = "Lezione_15/Jsons/config.json" # SELEZIONARE NOME DA MODIFICARE/CREARE
 mode: str = "r"
 encoding: str = "utf-8"
 
-config = None
 
-def connect(host, port):
 
-    #
-    print(f"Connected to {host}:{port}")
+# MODIFICARE UN FILE -INIZIO-----------------------
+# config = None
 
-with open(PATH, mode=mode, encoding=encoding) as file:
+# def connect(host, port):
 
-    config: dict = json.load(file)
+#     #
+#     print(f"Connected to {host}:{port}")
 
-config["AGGIUNTA"] = "NUOVO"
-config["server"]["host"] = "google.it"
+# with open(PATH, mode=mode, encoding=encoding) as file:
+
+#     config: dict = json.load(file)
+
+# config["AGGIUNTA"] = "NUOVO"
+# config["server"]["host"] = "google.it"
+# MODIFICARE UN FILE -FINE-----------------------
+
+
+
+# CREARE UN FILE -INIZIO-----------------------
+config: dict = {"GRGFLV...": {"name": "Dioni", "last_name": "Manon", "age": 40}}
+# CREARE UN FILE -FINE-----------------------
+
+
 
 with open(PATH, mode="w") as file:
 
     json.dump(config, file, indent=4) # =buttalo
+
+
 
 # #
 # print(f'Host: {config["server"]["host"]} Port: {config["server"]["port"]}')
