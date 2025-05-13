d343ebacf284162d10793f6f01d75fcb8df56773
unFake84 <nat_z@hotmail.it>
2025-05-14 00:31:07 +0200

    140525

--- Lezione_04/Collatz/esercizio3.py
+++ Lezione_04/Collatz/esercizio3.py
@@ -6,6 +6,17 @@ lista di num int disordinata restituisce una lista di interi ordinata dal piu pi
 
 def ordinalista(lista: list[int]) -> list[int]:
 
-    for iteratore in len(lista):
+    lista_ordinata: list[int] = []
 
-        
+    for i in range(len(lista)):
+
+        for j in range(lista[i]):
+
+            lista_ordinata.append(i)
+
+    return lista_ordinata
+
+if __name__ == "__main__":
+
+    lista_disordinata: list[int] = ordinalista([5, 2, 8, 6, 7, 1, 12, 9, 3])
+    print(lista_disordinata)
