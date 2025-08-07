08a7a3959cf29fd6c82a2b48df12670abe15a727
unFake84 <nat_z@hotmail.it>
2025-08-07 02:56:30 +0200

    070825

--- Recupero e potenziamento 2/esercizio1.py
+++ Recupero e potenziamento 2/esercizio1.py
@@ -68,13 +68,8 @@ La massima lunghezza è 0
 
 '''
 
-def isDNA( seq: str ) -> bool:
-    '''
-    Scrivere una funzione isDNA() che, data in input una sequenza,
-    restituisca True se la sequenza passata è una valida sequenza del DNA formata dai soli caratteri
-    A, C, G o T, e che restituisca False altrimenti.
-    Può essere utile usare una regex.
-    '''
+def isDNA( seq:str ) -> bool:
+
     if not isinstance( seq, str ):
         return False
 
@@ -85,17 +80,65 @@ def isDNA( seq: str ) -> bool:
 
     return True
 
-def fusionDNA( s1: str, s2: str ) -> str:
-    pass
+def fusionDNA( s1:str , s2:str ) -> str:
 
-# if __name__ == "__main__":
+    slicer_s1: int = -1
+    slicer_s2: int = 1
+    max_length: int = 0
 
-#     sequenza: str = ""
-#     n_sequenza: int = 1
+    for _ in s1:
+        if s1[ slicer_s1: ] == s2[ :slicer_s2 ]:
+            max_length = slicer_s2
 
-#     while True:
+        slicer_s1 -= 1
+        slicer_s2 += 1
 
-#         user: str = input( f"Inserire sequenza n° { n_sequenza } :" )
-#         print( isDNA( user ) )
-#         sequenza = user
-#         print( sequenza )
+    final_string: str = f"{ s1[ :slicer_s1 ] }{ s1[ slicer_s1: ] }\n"\
+                        f"{' ' * len( s1[ max_length: ] ) }"\
+                        f"{ s2[ slicer_s2: ] }{ s2[ :slicer_s2 ] }\n"\
+                        f"La massima lunghezza di sovrapposizione è: { max_length }.\n"
+
+    return final_string
+
+def testDNA() -> None:
+
+    # TEST A
+    dna_1a: str = "TTGACCAGGTCA"
+    dna_2a: str = "AACCGGTTAA"
+
+    # TEST B
+    dna_1b: str = "GGTACCGTGA"
+    dna_2b: str = "CGTGAACCTT"
+
+    # TEST C
+    dna_1c: str = "AAGCTTACG"
+    dna_2c: str = "ACGTTCGA"
+
+    # TEST D
+    dna_1d: str = "TTACGAGTACGCTAGT"
+    dna_2d: str = "ACGCTAGTCCGA"
+
+    # TEST E
+    dna_1e: str = "AGCTAACG"
+    dna_2e: str = "AACGTTCGA"
+
+    # TEST F
+    dna_1f: str = "AAAA"
+    dna_2f: str = "AAAA"
+
+    # TEST G
+    dna_1g: str = "ACGT"
+    dna_2g: str = "ATGC"
+
+    # TESTS EXEC
+    print( fusionDNA( dna_1a, dna_2a ) , "-" * 50 ) # A
+    print( fusionDNA( dna_1b, dna_2b ) , "-" * 50 ) # B
+    print( fusionDNA( dna_1c, dna_2c ) , "-" * 50 ) # C
+    print( fusionDNA( dna_1d, dna_2d ) , "-" * 50 ) # D
+    print( fusionDNA( dna_1e, dna_2e ) , "-" * 50 ) # E
+    print( fusionDNA( dna_1f, dna_2f ) , "-" * 50 ) # F
+    print( fusionDNA( dna_1g, dna_2g ) , "-" * 50 ) # G
+
+if __name__ == "__main__":
+
+    testDNA()
