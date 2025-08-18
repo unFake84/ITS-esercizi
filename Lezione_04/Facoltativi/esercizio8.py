1ccc545c514d7ca635aebc38b190df9abb9f0e6d
unFake84 <nat_z@hotmail.it>
2025-08-18 18:51:36 +0200

    om1850

--- Lezione_04/Facoltativi/esercizio8.py
+++ Lezione_04/Facoltativi/esercizio8.py
@@ -9,21 +9,81 @@ ATM Machine Simulator:
 
 '''
 
-def account(id: int, name: str, last_name: str, fund: int|float) -> tuple[int, str, str, int|float]:
+from datetime import datetime
 
-    t: tuple[int, str, str, int|float] = (id, name, last_name, fund)
+def account(
+    id: int,
+    name: str,
+    last_name: str,
+    fund: int|float
+    ) -> tuple[int, str, str, int|float, list[tuple[str, int|float, datetime]]]:
+
+    now: datetime = datetime.now()
+    operation: tuple[str, int|float, datetime] = ("Deposit", fund, now.strftime("%Y-%m-%d %H:%M:%S"))
+
+    t: tuple[int, str, str, int|float, list[tuple[str, int|float, datetime]]] = (
+    id,
+    name,
+    last_name,
+    fund,
+    [operation]
+    )
+
     return t
 
-def bank():
-    pass
+def bank(
+    database: list[tuple[int, str, str, int|float, list[tuple[str, int|float, datetime]]]],
+    account: tuple[int, str, str, int|float, list[tuple[str, int|float, datetime]]]
+    ) -> list[tuple[int, str, str, int|float, list[tuple[str, int|float, datetime]]]]:
+
+    if database == []:
+        database.append(account)
+        return database
+
+    else:
+        flag: bool = False
+
+        for acc in database:
+
+            if acc[0] == account[0]:
+                flag = True
+
+        if not flag:
+            database.append(account)
+            return database
+
+        if flag:
+            raise ValueError("Account allready exists")
 
 def atm() -> float:
     pass
 
 if __name__ == "__main__":
 
+    database: list[tuple[int, str, str, int|float, list[tuple[str, int|float, datetime]]]] = []
+
     account_1: tuple[int, str, str, int|float] = account(1, "Mario", "Rossi", 20000)
     account_2: tuple[int, str, str, int|float] = account(2, "Pino", "Pano", 14000.15)
     account_3: tuple[int, str, str, int|float] = account(3, "Uccio", "Uccia", 44761)
     account_4: tuple[int, str, str, int|float] = account(4, "Romualdo", "Latrinini", 9700.86)
-    print(account_1)
+    account_5: tuple[int, str, str, int|float] = account(1, "Mario", "Rossi", 30000)
+
+    
+    bank(database, account_1)
+    bank(database, account_2)
+    bank(database, account_3)
+    bank(database, account_4)
+    # bank(database, account_5)
+    # bank(database, account_2)
+    print(database)
+
+
+
+
+
+
+# for i, a in enumerate(database):
+#     if a[0] == account[0]:
+#         sum_ab: int|float = a[3] + account[3]
+#         temp_tuple: tuple[int, str, str, int|float] = (account[0], account[1], account[2], sum_ab)
+#         database[i] = temp_tuple
