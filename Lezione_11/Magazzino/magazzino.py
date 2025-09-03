f9afc6cec48198c23616593886cfb55113d71442
unFake84 <nat_z@hotmail.it>
2025-09-03 12:26:18 +0200

    030925

--- Lezione_11/Magazzino/magazzino.py
+++ Lezione_11/Magazzino/magazzino.py
@@ -39,6 +39,9 @@ class Prodotto:
         self.nome = n
 
     def set_quantita(self, q: int) -> None:
+        if q <= 0:
+            raise ValueError("La quantità non può essere negativa")
+
         self.quantita = q
 
     def get_nome(self) -> str:
@@ -49,7 +52,7 @@ class Prodotto:
 
     def __str__(self) -> str:
         str_prodotto: str = f"{'Nome: ':<10}{self.get_nome()}\n"\
-                            f"{'Quantita ':<10}{self.get_quantita()}"
+                            f"{'Quantita: ':<10}{self.get_quantita()}"
 
         return str_prodotto
 
@@ -59,7 +62,6 @@ class Magazzino:
         - aggiungi_prodotto(prodotto: Prodotto): aggiunge un prodotto al magazzino.
         - cerca_prodotto(nome: str) -> Prodotto: cerca un prodotto per nome e lo ritorna se esiste.
         - verifica_disponibilità(nome: str) -> str: verifica se un prodotto è disponibile in magazzino.
-
     '''
 
     nome: str
@@ -80,17 +82,15 @@ class Magazzino:
         - aggiunge un prodotto al magazzino
         '''
 
-        if prodotto not in self.lista_prodotti:
-            self.lista_prodotti.append(prodotto)
+        for p in self.lista_prodotti:
 
-        else:
-            for p in self.lista_prodotti:
+            if p.get_nome() == prodotto.get_nome():
+                p.set_quantita(prodotto.get_quantita() + p.get_quantita())
+                return
 
-                if p.get_nome() == prodotto.get_nome():
-                    p.set_quantita(prodotto.get_quantita() + p.get_quantita())
-                    break
+        self.lista_prodotti.append(prodotto)
 
-    def cerca_prodotto(self, nome: str) -> Prodotto:
+    def cerca_prodotto(self, nome: str) -> Prodotto|None:
         '''
         - cerca un prodotto per nome e lo ritorna se esiste.
         '''
@@ -115,6 +115,18 @@ class Magazzino:
                 return prodotto_str
         
         return f"Nessun prodotto {nome} nell'inventario"
+    
+    def __str__(self) -> str:
+        tot_qnt: int = 0
+
+        for ogg in self.lista_prodotti:
+
+            tot_qnt += ogg.quantita
+
+        ogg_magaz: str = f"\n{'-'*25}\n" + f"\n{'-'*25}\n".join(f'{"prodotto ":<10}[{i}]\n' + str(n) for i, n in enumerate(self.lista_prodotti, 1)) + f"\n{'-'*25}"
+        magaz_str: str = f"{'-'*25}\n{'Magazzino':<10}{self.nome}\n{ogg_magaz}\n\nTot oggetti: {len(self.lista_prodotti)}\nQuantità tot: {tot_qnt}"
+
+        return magaz_str
 
 if __name__ == "__main__":
 
@@ -122,21 +134,36 @@ if __name__ == "__main__":
     prodotto2: Prodotto = Prodotto("Astuccio", 20)
     prodotto3: Prodotto = Prodotto("Quaderno", 1)
 
-    print(prodotto1.get_quantita())
-    prodotto1.set_quantita(15)
-    print(prodotto1.get_quantita())
+    #print(prodotto1.get_quantita())
+    prodotto1.set_quantita(15)                  #1 prodotto1, quantita = 15
+    #print(prodotto1.get_quantita())
 
     magazzino1: Magazzino = Magazzino("Mgz1")
     magazzino1.aggiungi_prodotto(prodotto1)
-    magazzino1.aggiungi_prodotto(prodotto1)
+    magazzino1.aggiungi_prodotto(prodotto1)     #2 prodotto1, quantita = 30
 
-    magazzino1.aggiungi_prodotto(prodotto2)
-    print(prodotto1.get_quantita())
+    magazzino1.aggiungi_prodotto(prodotto2)     #3 prodotto2(20) + prodotto1(30) = 50
+    #print(prodotto1.get_quantita())            #4 = 50
 
-    print(magazzino1.verifica_disponibilita("Astuccio"))
+    #print(magazzino1.verifica_disponibilita("Astuccio"))
     magazzino1.aggiungi_prodotto(prodotto3)
-    print(magazzino1.verifica_disponibilita("Quaderno"))
-    print(magazzino1.verifica_disponibilita("Diario"))
+    #print(magazzino1.verifica_disponibilita("Quaderno"))
+    #print(magazzino1.verifica_disponibilita("Diario"))
 
 
-    print(magazzino1.cerca_prodotto("Quaderno"))
+    #print(f"Hai cercato:\n{magazzino1.cerca_prodotto('Quaderno')}")
+
+
+    prodotto4: Prodotto = Prodotto("Matita", 100)
+    prodotto5: Prodotto = Prodotto("Penna", 200)
+    prodotto6: Prodotto = Prodotto("Righello", 36)
+    prodotto7: Prodotto = Prodotto("Compasso", 60)
+    prodotto8: Prodotto = Prodotto("Evidenziatore", 140)
+
+    lista_prodotti: list[Prodotto] = [prodotto4, prodotto5, prodotto6, prodotto7, prodotto8]
+    
+    for prodotto in lista_prodotti:
+
+        magazzino1.aggiungi_prodotto(prodotto)
+
+    print(magazzino1)
